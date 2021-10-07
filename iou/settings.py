7df2132d922fdd49d9b14dc13f2ca336e390d9c9
gallimattias <48020475+gallimattias@users.noreply.github.com>
2021-10-07 12:16:43 +0200

    Adopting homepage to Django and initial Model

--- iou/settings.py
+++ iou/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'localflavor',
-    'phonenumber_field'
+    'phonenumber_field',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
